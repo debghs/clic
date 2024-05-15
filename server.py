12771bf23b01846cbe171a2372de396c9f5083e3
debghs <dghosh31428@gmail.com>
2024-05-16 01:06:19 +0530

    bug fix
    index out of bound error for empty private messages has been fixed

--- server.py
+++ server.py
@@ -22,7 +22,10 @@ class ChatServer:
                     break
                 elif message.startswith('@'):
                     recipient, msg_content = self.parse_private_message(message)
-                    self.send_private_message(username, recipient, msg_content)
+                    if recipient and msg_content:
+                        self.send_private_message(username, recipient, msg_content)
+                    else:
+                        client_socket.send("Invalid private message format. Use '@username message'.\n".encode('utf-8'))
                 else:
                     self.broadcast(username + ': ' + message)
             except Exception as e:
@@ -40,9 +43,12 @@ class ChatServer:
 
     def parse_private_message(self, message):
         parts = message.split(' ', 1)
-        recipient = parts[0][1:]
-        msg_content = parts[1]
-        return recipient, msg_content
+        if len(parts) == 2:
+            recipient = parts[0][1:]
+            msg_content = parts[1]
+            return recipient, msg_content
+        else:
+            return None, None
 
     def send_private_message(self, sender, recipient, message):
         if recipient in self.clients:
@@ -82,4 +88,3 @@ class ChatServer:
 if __name__ == "__main__":
     server = ChatServer('0.0.0.0', 5555)
     server.start()
-
